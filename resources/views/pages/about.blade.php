5438afad4362c58ceae02f85ca0f692b10bc66c9
John Sneddon <sneddon.j89@gmail.com>
2017-11-15 13:53:34 +0000

    Added Bootstrap Navbar and some layout

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,15 +1,6 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>{{config('app.name', 'LGSAPP')}}</title>
-        <style>
-        </style>
-    </head>
-    <body>
+@extends('layouts.app')
+
+@section('content')
     <h1>About</h1>
     <p>This is the About page</p>
-    </body>
-</html>
+@endsection
