cdb71f35f82f9106e197517d95bdcf914fc670d3
John Sneddon <sneddon.j89@gmail.com>
2017-11-15 11:56:25 +0000

    Initial setup complete

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>{{config('app.name', 'LGSAPP')}}</title>
+        <style>
+        </style>
+    </head>
+    <body>
+    <h1>Welcome to Laravel</h1>
+    <p>This is the Gym Slots Application inspired by Arnold Clarks Hack Day and Traversy Medias Laravel Tutorial</p>
+    </body>
+</html>
